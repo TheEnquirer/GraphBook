d8e2da40c0e0095b952dd671ab95d1f48b232cb3
Exr0n <mail@exr0n.com>
2022-07-29 02:54:18 -0700

    the spacy-nlp js package seems promising ... making super simple for hux
    appasement first

--- jentity_linker/index.mjs
+++ jentity_linker/index.mjs
@@ -17,7 +17,7 @@ import spacy from 'spacy-js';
 //
 // options for nlp libs
 // - spacy: js integration is scuffed, certainly need a python server somewhere; spacy-js seems broken so lets try spacy-nlp
-// - wink-nlp: js first, but it certainly isn't as complete as spacy and may not have dependencies
+// - wink-nlp: js first, but it certainly isn't as complete as spacy and may not have dependencies... i suspect wink won't be able to get us dependencies
 // - compromise (https://github.com/spencermountain/compromise): browser first, similar limitations to wink (but it looks like an even smaller ecosystem). compromise has a limited wordlist so it probably won't be able to lemmatize textbooks which have obscure words
 
 class Linker {
@@ -41,13 +41,19 @@ class Linker {
 const linker = new Linker();
 const text = `Every real number equals its com- plex conjugate. Thus if we are dealing with a real vector space, then in the last condition above we can dispense with the complex conjugate.`;
 
-(async function() {
-    const nlp = spacy.load('en_core_web_sm');
-    const doc = await nlp('This is a text about Facebook.');
-    for (let ent of doc.ents) {
-        console.log(ent.text, ent.label);
-    }
-    for (let token of doc) {
-        console.log(token.text, token.pos, token.head.text);
-    }
-})();
+import spacyNLP from 'spacy-nlp';
+// default port 6466
+// start the server with the python client that exposes spacyIO (or use an existing socketIO server at IOPORT)
+var serverPromise = spacyNLP.server({ port: process.env.IOPORT });
+// Loading spacy may take up to 15s
+
+//(async function() {
+//    const nlp = spacy.load('en_core_web_sm');
+//    const doc = await nlp('This is a text about Facebook.');
+//    for (let ent of doc.ents) {
+//        console.log(ent.text, ent.label);
+//    }
+//    for (let token of doc) {
+//        console.log(token.text, token.pos, token.head.text);
+//    }
+//})();
