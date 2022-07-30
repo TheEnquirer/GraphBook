83ecc1c51783faa6b39aab9f5ad2804139541044
Exr0n <mail@exr0n.com>
2022-07-30 01:43:53 -0700

    how tf to flat

--- textbooks_done_right/src/App.js
+++ textbooks_done_right/src/App.js
@@ -4,168 +4,214 @@ import parse from 'html-react-parser';
 import book from "./raw_book.jsx";
 import { useEffect, useState, createElement } from 'react';
 import reactHtmlReplace from 'react-html-replace';
-import React, { Component } from 'react';
+import React, { Component, useMemo } from 'react';
 import _ from 'lodash';
 import * as ReactDOM from 'react-dom'
 import { Virtuoso } from 'react-virtuoso'
 
-import { TWELVE } from 'jentity_linker';
+import { TWELVE, DummyAhoLinker } from './jentity_linker';
+
+// hewwwuxxxxxxxxxxxx heres an example of how to use my thingy
+function TextWithHovers({ text }) {
+    const definitions = {
+        '12': TWELVE,
+        'complex': "a complex number.", // you could also use Symbols here and have your own jsx object stored elsewhere as the definition
+        'complex conjugate': "a unary operation that negates the complex component of a complex number."
+    };
+
+    const parser = useMemo(() => {
+        const linker = new DummyAhoLinker();
+        Object.entries(definitions).forEach(([name, def]) => linker.register_definition.apply(linker, [name, def]));
+        return linker.find_links.bind(linker);
+    }, [definitions]);
+    // parser.find_links(text) -> immutable.List([ [beg_idx_inc, end_idx_exc, '12', 'twelve.'], etc ])
+    // find_links is as fast as i can think to make it, roughly linear on text.length + sum(definitions.keys().map(length)) possibly still worth useMemo though
+
+    const parsed = useMemo(() => parser(text), [text]);
+    console.log(parsed)
+    const end_indicies = parsed.map(([b, e, n, d]) => [b, e]).flatten(0);
+    const beg_indicies = end_indicies.unshift(0);    // might need to .delete() but thats O(N) => cringe
+
+    console.log('end_indicies', end_indicies)
+    console.log('beg_indicies', beg_indicies.toJS())
+
+    const sections = parsed.map(seg => [null, seg]).push(null).flatten(0)   // intersperse with nulls for the non-highlighted sections
+        //.zip(beg_indicies, end_indicies).map(([hover, beg, end], i) =>
+        //    <span key={i} onMouseEnter={hover ? console.log(hover) : () => {}}>{text.slice(beg, end)}</span>
+        //)
+        .zip(beg_indicies, end_indicies).map(([hover, beg, end], i) => {
+            console.log("eeeeeeeeeee", beg, end, text.slice(beg, end), hover)
+            return <span key={i} onMouseEnter={hover ? console.log(hover) : () => {}}>{text.slice(beg, end)}</span>
+        })
+        .toJS();
+    console.log(sections);
+    return <div style={{ border: '2px solid red' }}>hewwwwwwwwwwwwwwwww{sections}</div>;
+
+    // sorry if that example wasn't very helpful. i just got in a functional kinda headspace yk
+    // the zip offset thing is pretty scuffed i'll admit
+    // also the returned segments can overlap, so my example just kinda duplicates the overlapped text. idk how u wanna deal with that
+    // the overlap can occur because if you have "real number" and "number equals" in the dictionary, then they might be the same length and it's not clear which one should get a hover. so this example would just spit out <span onHover>real number</span><span onHover>number equals</span>
+}
 
 function App() {
     console.log('twelve?', TWELVE);
 
-    const bookParser = (book) => {
-	// UTILS //
-	const isObj = (el) => (
-	    typeof el === 'object' &&
-	    !Array.isArray(el) &&
-	    el !== null
-	)
-
-	// CHECKS //
-
-         // BLUE //
-	let inBlue = false;
-	let bIdx = -1;
-	let blues = []
-	const checkBlue = (obj) => {
-	    let localVal = null;
-	    if (obj.props && obj.props.className) {
-		if (obj.props.className.split(" ").includes("fca") && inBlue == false) {
-		    localVal = "start"
-		}
-
-		else if (
-		    obj.props.className.split(" ").includes("fc7")
-		    || obj.props.className.split(" ").includes("x9")
-		    || obj.props.className.split(" ").includes("x69")
-		) {
-		    localVal = "end"
-		}
-	    }
-
-	    if (localVal === "start") {
-		inBlue = true;
-		bIdx++;
-		blues.push([])
-	    }
-
-	    if (localVal === "end") {
-		inBlue = false
-	    }
-
-	    if (inBlue) {
-		blues[bIdx].push(obj)
-	    }
-
-	    return localVal
-	}
-
-	let defIdx = -1;
-	let defs = []
-	let inDef = false;
-
-	const checkDef = (obj) => {
-	    let localVal = null;
-	    if (obj.props && obj.props.className) {
-		if (obj.props.className.split(" ").includes("fc9")) {
-		    localVal = "start"
-		}
-
-		else if (obj.props.className.split(" ").includes("x4") || obj.props.className.split(" ").includes("x9")  || obj.props.className.split(" ").includes("x69")) {
-		    localVal = "end"
-		}
-	    }
-
-	    if (localVal === "start") {
-		inDef = true;
-		defIdx++;
-		defs.push([])
-	    }
-
-	    if (localVal === "end") {
-		inDef = false
-	    }
-
-	    if (inDef) {
-		defs[defIdx].push(obj)
-	    }
-	}
+    return <TextWithHovers text={ `Every real number equals its complex conjugate. Thus if we are dealing with a real vector space, then in the last condition above we can dispense with the complex conjugate.` } />
 
 
-    	// MAIN RECURSION LOOP //
-	const search = (obj) => {
 
-	    checkBlue(obj);
-	    checkDef(obj);
+    const bookParser = (book) => {  // nice -alb
+        // UTILS //
+        const isObj = (el) => (
+            typeof el === 'object' &&
+            !Array.isArray(el) &&
+            el !== null
+        )
 
-	    // recurse
-	    if (obj.props && obj.props.children) {
-		if (isObj(obj.props.children)) {
-		    search(obj.props.children)
-		} else {
-		    //if (obj.props.children.push) obj.props.children.push("huh?")
-		    for (const i of obj.props.children) {
-			search(i)
-		    }
-		}
-	    }
-	}
+        // CHECKS //
 
-	let repChildren = book.props.children[1].props.children[3].props.children
-	book.props.children[1].props.children[3].props.children = [<Virtuoso
-	    style={{ height: "800px", }}
-	    totalCount={200}
-	    //itemContent={(index) => <div>Item {index}</div>}
-	    itemContent={(index) => {
-		return <div style={{minHeight: "1px"}}> {repChildren[index]} </div>
-	    }}
-	/>]
+        // BLUE //
+        let inBlue = false;
+        let bIdx = -1;
+        let blues = []
+        const checkBlue = (obj) => {
+            let localVal = null;
+            if (obj.props && obj.props.className) {
+                if (obj.props.className.split(" ").includes("fca") && inBlue == false) {
+                    localVal = "start"
+                }
+
+                else if (
+                    obj.props.className.split(" ").includes("fc7")
+                    || obj.props.className.split(" ").includes("x9")
+                    || obj.props.className.split(" ").includes("x69")
+                ) {
+                    localVal = "end"
+                }
+            }
+
+            if (localVal === "start") {
+                inBlue = true;
+                bIdx++;
+                blues.push([])
+            }
+
+            if (localVal === "end") {
+                inBlue = false
+            }
+
+            if (inBlue) {
+                blues[bIdx].push(obj)
+            }
+
+            return localVal
+        }
+
+        let defIdx = -1;
+        let defs = []
+        let inDef = false;
+
+        const checkDef = (obj) => {
+            let localVal = null;
+            if (obj.props && obj.props.className) {
+                if (obj.props.className.split(" ").includes("fc9")) {
+                    localVal = "start"
+                }
+
+                else if (obj.props.className.split(" ").includes("x4") || obj.props.className.split(" ").includes("x9")  || obj.props.className.split(" ").includes("x69")) {
+                    localVal = "end"
+                }
+            }
+
+            if (localVal === "start") {
+                inDef = true;
+                defIdx++;
+                defs.push([])
+            }
+
+            if (localVal === "end") {
+                inDef = false
+            }
+
+            if (inDef) {
+                defs[defIdx].push(obj)
+            }
+        }
 
 
-	search(book)
-	//console.log(blues)
-	//console.log(
-	//props.children[1].props.children[3].props.children
+        // MAIN RECURSION LOOP //   // :eyes: -alb
+        const search = (obj) => {
 
-	//const element = React.createElement(
-	//    'h1',
-	//    {className: 'greeting'},
-	//    'Hello, world!'
-	//);
+            checkBlue(obj);
+            checkDef(obj);
 
-	return book
-	//NW TODO: we append the page svgs
+            // recurse
+            if (obj.props && obj.props.children) {
+                if (isObj(obj.props.children)) {
+                    search(obj.props.children)
+                } else {
+                    //if (obj.props.children.push) obj.props.children.push("huh?")
+                    for (const i of obj.props.children) {
+                        search(i)
+                    }
+                }
+            }
+        }
+
+        let repChildren = book.props.children[1].props.children[3].props.children
+        book.props.children[1].props.children[3].props.children = [<Virtuoso
+            style={{ height: "800px", }}
+            totalCount={200}
+            //itemContent={(index) => <div>Item {index}</div>}
+            itemContent={(index) => {
+                return <div style={{minHeight: "1px"}}> {repChildren[index]} </div>
+            }}
+            />]
+
+
+        search(book)
+        //console.log(blues)
+        //console.log(
+        //props.children[1].props.children[3].props.children
+
+        //const element = React.createElement(
+        //    'h1',
+        //    {className: 'greeting'},
+        //    'Hello, world!'
+        //);
+
+        return book
+        //NW TODO: we append the page svgs
     }
 
     const [foundBlues, setBlues] = useState([]);
     const [displayParsed, setParsed] = useState("");
 
     const reconstruct = (book) => {
-	return _.cloneDeep(book)
-	//return book
+        return _.cloneDeep(book)
+        //return book
     }
 
     useEffect(() => {
-	let parsed = reconstruct(parse(book))
-	setParsed(bookParser(parsed))
+        let parsed = reconstruct(parse(book))
+        setParsed(bookParser(parsed))
     }, [])
 
-	    //{foundBlues.map((blue, idx) => {
-	    //    return blue.map((el, idx) => {
-	    //        return el
-	    //    })}
-	    //)}
+    //{foundBlues.map((blue, idx) => {
+    //    return blue.map((el, idx) => {
+    //        return el
+    //    })}
+    //)}
 
     return (
-	<>
-	      {/*return <Virtuoso
+        <>
+        {/*return <Virtuoso
     style={{ height: "400px", }}
     totalCount={200000}
     itemContent={(index) => <div>Item {index}</div>}
   />*/}
-	    {displayParsed}
-	</>
+        {displayParsed}
+        </>
     );
 }
 
---
+++ textbooks_done_right/src/jentity_linker.mjs
@@ -0,0 +1,100 @@
+import FastScanner from 'fastscan';
+import { List } from 'immutable';
+
+import { useMemo } from 'react';
+
+export const TWELVE = 'twelve.';    // very important
+
+export class DummyAhoLinker {
+    constructor() {
+        this.scanner = new FastScanner([]);
+        this.words = [];
+        this.dictionary = {};
+    }
+    register_definition(name, definition) {
+        this.dictionary[name] = definition;
+        this.words.push(name);
+        this.scanner = new FastScanner(this.words);
+    }
+    find_links(text) {
+        return this.scanner.search(text)
+            // replace previous if idx is the same, append if it isn't
+            .reduce((a, c) => a.size > 0 && a.last().first() == c[0] ? a.set(a.size-1, List(c)) : a.push(List(c)), List())
+            // map to the output format of [begin_inc, end_exc, name, definition]
+            .map(([idx, name]) => [idx, idx+name.length, name, this.dictionary[name]]);
+    }
+}
+
+const linker = new DummyAhoLinker();
+const text = `Every real number equals its complex conjugate. Thus if we are dealing with a real vector space, then in the last condition above we can dispense with the complex conjugate.`;
+
+//console.log(linker.find_links(text))
+//linker.register_definition('complex', 'a complex number')
+//console.log(linker.find_links(text))
+//linker.register_definition('complex conjugate', 'a complex number but with the negative part flipped')
+//console.log(linker.find_links(text))
+
+
+
+// what happens when "complex number" is shortened to "complex"?
+// can we get away by just including a sorted list of lemmas?
+
+// can we allow overlapping matches and non-contiguous matches?
+//  currently the dummyaholinker allows overlapping, but not non-contiguous
+//  non-contiguous could be useful for when a word modifies an earlier part of the sentence
+
+// ideating
+// hash-trie of lematized words that refer to other words? for quick search
+// eg. "commutativity of scalar addition" -> { addition: { scalar: commutativity: Symbol } }
+//
+// but how to deal with associativity?
+// commutativity of (scalar addition) vs smt else??
+// (conjugate symmetry) of (inner products), vs conjugate (symmetry of inner products)
+//
+// maybe i'll need to store serialized keys as trees... but lets see whether we can even do lemmatization, POS, and referencing first
+
+// looks like spacy has dependency parsing ... but the js package still runs nlp in python ... figures
+//
+//
+// options for nlp libs
+// - spacy: js integration is scuffed, certainly need a python server somewhere; spacy-js seems broken so lets try spacy-nlp
+// - wink-nlp: js first, but it certainly isn't as complete as spacy and may not have dependencies... i suspect wink won't be able to get us dependencies
+// - compromise (https://github.com/spencermountain/compromise): browser first, similar limitations to wink (but it looks like an even smaller ecosystem). compromise has a limited wordlist so it probably won't be able to lemmatize textbooks which have obscure words
+
+//class Linker {
+//    constructor() {
+//        this.dictionary = {};
+//        this.match_trie = {};
+//    }
+//    async parse_text(text) {
+//        // TODO
+//    }
+//    async register_definition(name, definition) {
+//        const sem = parse_text(name);
+//        this.dictionary[name] = definition;
+//
+//        for (const tok of sem) {    // or smt TODO
+//            this.match_trie[tok] = 3;   // TOOD
+//        }
+//    }
+//}
+//
+//const linker = new Linker();
+//const text = `Every real number equals its com- plex conjugate. Thus if we are dealing with a real vector space, then in the last condition above we can dispense with the complex conjugate.`;
+//
+//import spacyNLP from 'spacy-nlp';
+//// default port 6466
+//// start the server with the python client that exposes spacyIO (or use an existing socketIO server at IOPORT)
+//var serverPromise = spacyNLP.server({ port: process.env.IOPORT });
+//// Loading spacy may take up to 15s
+//
+////(async function() {
+////    const nlp = spacy.load('en_core_web_sm');
+////    const doc = await nlp('This is a text about Facebook.');
+////    for (let ent of doc.ents) {
+////        console.log(ent.text, ent.label);
+////    }
+////    for (let token of doc) {
+////        console.log(token.text, token.pos, token.head.text);
+////    }
+////})();
